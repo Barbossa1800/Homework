9e5bcf989c66aff4c7ec0bdd8ca59a31b176cd74
Медко Никита <nikita.jove28@gmail.com>
2020-10-11 13:57:24 +0300

    Lab 7 done

---
+++ Homework/Lab7/Lists.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab7
+{
+    public class Lists
+    {
+        private List<string> values;
+        public Lists()
+        {
+            values = new List<string>();
+        }
+        public Lists(List<string> list)
+        {
+            if (list == null || list.Count == 0)
+                throw new ArgumentNullException(); // исключение 
+            values = list;
+        }
+
+        public List<string> Values => values; // паблик свойство 
+        public void AddValue(string value)
+        {
+            values.Add(value);
+        }
+        public int GetIndexByValue(string value)
+        {
+            return values.IndexOf(value);
+        }
+        public string[] GetListAsArray()
+        {
+            return values.ToArray();
+        }
+    }
+}
