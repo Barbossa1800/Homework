747f249428c68145a0b2e340cfe30781005dc919
Медко Никита <nikita.jove28@gmail.com>
2020-10-11 20:41:30 +0300

    Change Test

--- Homework/TestLab/Program.cs
+++ Homework/TestLab/Program.cs
@@ -13,51 +13,38 @@ namespace TestLab
             Random random = new Random();
             int rand;
             int sum = 0;
+            for (int i = 0; i < N; i++)
             {
+                for (int j = 0; j < N; j++)
                 {
-                    for (int i = 0; i < N; i++)
-                    {
-                        for (int j = 0; j < N; j++)
-                        {
-                            rand = random.Next(1, 3);
-                            array[i, j] = rand;
-                            Console.Write($"{array[i, j]} \t");
+                    rand = random.Next(1, 3);
+                    array[i, j] = rand;
+                    Console.Write($"{array[i, j]} \t");
 
-                        }
-                        Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+            for (int i = 0; i < N; i++)
+            {
+                sum += array[i, i];
+            }
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+
+                    if (j == N - i - 1)
+                    {
+                        sum += array[i, j];
                     }
+                    if (N % 2 != 0 && i == N / 2 + 1 && j == N / 2 + 1)
                     {
-                        {
-                            for (int i = 0; i < N; i++)
-                                sum += array[i, i];
-
-                        }
-                    }
-                    {
-                        {
-                            for (int i = 0; i < N; i++)
-                            {
-                                for (int j = 0; j < N; j++)
-                                {
-
-                                    if (j == N - i - 1)
-                                    {
-                                        sum += array[i, j];
-                                    }
-                                    if (N % 2 != 0 && i == N / 2 + 1 && j == N / 2 + 1)
-                                    {
-                                        sum -= array[N / 2 + 1, N / 2 + 1];
-                                    }
-                                }
-                            }
-                        }
+                        sum -= array[N / 2 + 1, N / 2 + 1];
                     }
                 }
-
-                Console.Write(sum);
-                Console.ReadKey();
-
             }
+            Console.Write(sum);
+            Console.ReadKey();
         }
     }
 }
