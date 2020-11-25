4c8b32f45413c26ffab27c96087b6e2f1a1a3df6
Медко Никита <nikita.jove28@gmail.com>
2020-11-25 20:01:57 +0200

    Lab 6 completed Beginning of 10 labs

--- Homework/Lab6/Program.cs
+++ Homework/Lab6/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Lab6.FirstPart;
+using Lab6.SecondPart;
 using System;
+using System.Text.RegularExpressions;
 
 namespace Lab6
 {
@@ -7,15 +9,22 @@ namespace Lab6
     {
         static void Main(string[] args)
         {
-            //Number number = new Number();
-            //number.RecursionNumber(new int[] { 1,2,3,4,5,5});
-            //number.RecursionNumber(123);
-            //Line line = new Line();
-            //line.RecursionString(new string[] { "God" });
-            //line.RecursionString();
-            //DoubleNumber doubleNumber = new DoubleNumber();
-            //doubleNumber.Sort();
-
+            Number number = new Number();
+            number.RecursionNumber(new int[] { 1, 2, 3, 4, 5, 5 });
+            number.RecursionNumber(123);
+            Line line = new Line();
+            line.RecursionString(new string[] { "God" });
+            line.RecursionString();
+            DoubleNumber doubleNumber = new DoubleNumber();
+            doubleNumber.Sort();
+            MagicSing magicSing = new MagicSing();
+            magicSing.MagicRecursion(new string[2] { "Nikita", "Dut" });
+            Don_tReverse don_TReverse = new Don_tReverse();
+            don_TReverse.DoReveres();
+            int[] num;
+            don_TReverse.DoReveres3(out num);
+            num = new int[] { 1, 2, 3, 4 };
+            don_TReverse.DoReveres4(ref num);
         }
 
     }
