8dc5be88480ae6f8140d04d20c2d980f018faed7
Медко Никита <nikita.jove28@gmail.com>
2020-11-25 21:13:36 +0200

    Lab work 10 is done

---
+++ Homework/Lab10/MyUtils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab10
+{
+    public static class MyUtils
+    {
+        public static void Num(this int[] num)
+        {
+            Array.Sort(num);
+            Array.Reverse(num);
+            foreach (var item in num)
+            {
+                Console.Write(item + " ");
+            }
+        }   
+    }
+}
--- Homework/Lab10/Program.cs
+++ Homework/Lab10/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
-
 namespace Lab10
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] num = { 1, 6, 0, 7, 5, 8, 3, 8, 5, 7, 4, 2, 4, 7, 3, 5, 7, 7, 4, 2, 9 };
+            MyUtils.Num(num);
         }
+
     }
 }
