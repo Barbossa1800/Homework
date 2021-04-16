7517f2a2c4ecfa7917ac6369adfdcf7eaf8ef4bd
Медко Никита <nikita.jove28@gmail.com>
2021-04-16 14:24:58 +0300

    completed tasks

---
+++ Homework/2_Sem/Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = {1,2,3,4};
+            foreach (var item in num)
+            {
+                Console.WriteLine(num[item]);
+                break;
+            }
+
+        }
+    }
+}
